de5ec6671686fc933a627e94eda4758da5ee7ea5
gRenka <128580487+gRenka-with-40k-garlic@users.noreply.github.com>
2023-10-15 14:22:28 +0700

    ┐(‘～` )┌

--- main.py
+++ main.py
@@ -3,14 +3,38 @@ import random
 array = [random.randint(1, 100) for _ in range(100)]
 
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+    return arr
+
+
+def inverse_insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+    return arr
+
+
 def sort_values(arr):
     even_values = [value for value in arr if value % 2 == 0]
     odd_values = [value for value in arr if value % 2 != 0]
 
-    even_values.sort()
-    odd_values.sort(reverse=True)
+    new_even_values = insertion_sort(even_values)
+    new_odd_values = inverse_insertion_sort(odd_values)
 
-    sorted_values = even_values + odd_values
+    sorted_values = new_even_values + new_odd_values
 
     return sorted_values
 
@@ -21,16 +45,20 @@ def sort_indices(arr):
     even_indices = [i for i in indices if i % 2 == 0]
     odd_indices = [i for i in indices if i % 2 != 0]
 
-    even_indices.sort()
-    odd_indices.sort(reverse=True)
+    new_even_indices = insertion_sort(even_indices)
+    new_odd_indices = inverse_insertion_sort(odd_indices)
 
-    sorted_indices = even_indices + odd_indices
+    sorted_indices = new_even_indices + new_odd_indices
 
     return sorted_indices
 
+print("Сгенерированный массив:", array)
 
-sorted_values = sort_values(array)
-print("Отсортированные значения:", sorted_values)
+sorted_arr = insertion_sort(array)
+print("Отсортированный массив:", sorted_arr)
+
+sorted_val = sort_values(array)
+print("Отсортированные значения:", sorted_val)
 
 sorted_indices = sort_indices(array)
 print("Отсортированные индексы:", sorted_indices)
